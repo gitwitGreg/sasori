7e8873adc9dfdfbd88c0e1bb53a512a746b89767
Greg <Godly@udel.edu>
2024-04-03 12:11:09 -0400

    able to create xlsx

--- scrape.js
+++ scrape.js
@@ -1,5 +1,9 @@
 import puppeteer from "puppeteer";
 import { writeFile } from "fs";
+import * as xlsx from 'xlsx/xlsx.mjs'
+import { json2csv } from "json-2-csv";
+import fs from 'fs'
+
 
 const googleFinance = 'https://www.google.com/finance/?hl=en'
 
@@ -7,14 +11,10 @@ const googleFinance = 'https://www.google.com/finance/?hl=en'
 const headless = async() => {
     try{
         const browser = await puppeteer.launch({
-            headless: false
+            headless: true
         });
 
         const page = await browser.newPage();
-        await page.setViewport({
-            width: 1080,
-            height: 1024,
-        });
 
         await page.goto(googleFinance);
         await page.waitForNetworkIdle();
@@ -54,19 +54,114 @@ const headless = async() => {
             };
         });
 
-        await writeFile('./data/mostActiveStocks', 
-        JSON.stringify(combinedData),
-        'utf-8', (error) => {
+
+        writeFile('./data/mostActiveStocks.json',
+            JSON.stringify(combinedData),
+            'utf-8', (error) => {
+                if (error) {
+                    console.log(error);
+                }
+        });
+
+        await page.click('.n4DWob');
+        await page.waitForNetworkIdle();
+
+
+        await page.click('.TkxCr')
+        await page.waitForNetworkIdle();
+        await page.waitForSelector('.SxcTic ')
+
+
+        const gainers = await page.evaluate(
+            () => {
+                const rows = document.querySelectorAll('.SxcTic ');
+                return Array.from(rows).map(stock => {
+                    return{
+                        companyName: stock.querySelector('.ZvmM7').textContent,
+                        price: stock.querySelector('.Bu4oXd').textContent,
+                        change: document.querySelector('.P2Luy.Ez2Ioe').textContent,
+                        growthPercent: stock.querySelector('.JwB6zf').textContent,
+                    }
+                })
+            }
+        );
+
+        writeFile('./data/topGainers.json',
+        JSON.stringify(gainers), 'utf-8',
+        (error) => {
+            if(error){
+                console.log('error saving file')
+            }
+        });
+
+        await page.waitForSelector('.GqNdIe')
+
+        const buttons = await page.$$('.GqNdIe');
+        const buttonToClick = buttons[buttons.length - 2];
+        
+        await buttonToClick.click();
+        await page.waitForNetworkIdle();
+        await new Promise(resolve =>  setTimeout(resolve, 4000))
+
+        const bitCoins= await page.evaluate(
+            () => {
+                const rows = document.querySelectorAll('.SxcTic ');
+                return Array.from(rows).map(stock => {
+                    return{
+                        companyName: stock.querySelector('.ZvmM7').textContent,
+                        price: stock.querySelector('.Bu4oXd').textContent,
+                        change: document.querySelector('.P2Luy.Ez2Ioe').textContent,
+                        growthPercent: stock.querySelector('.JwB6zf').textContent,
+                    }
+                })
+            }
+        );
+
+        const filteredCoins = bitCoins.slice(-75);
+
+        writeFile('./data/bitCoins.json', 
+        JSON.stringify(filteredCoins), 'utf-8',
+        (error) => {
             if(error){
                 console.log(error)
             }
-            console.log('Saved file');
-        });
+            console.log('bitcoin file saved')
+        })
+
+        await page.waitForNetworkIdle();
 
 
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        const bit2Csv = json2csv(filteredCoins);
+        const mostActiveCsv = json2csv(combinedData);
+        const gainsCsv = json2csv(gainers);
+
+        const parseCsv = (file) => {
+            return file.split('\n')
+            .map(row => row.split(','));
+        }
+
+        const bitParsed = parseCsv(bit2Csv);
+        const mostActiveParsed = parseCsv(mostActiveCsv);
+        const gainsParsed = parseCsv(gainsCsv)
+
+
+        const wb = xlsx.utils.book_new();
+
+        try{
+            const bit2sheet = xlsx.utils.aoa_to_sheet(bitParsed);
+            const mostActivesheet = xlsx.utils.aoa_to_sheet(mostActiveParsed);
+            const gainssheet = xlsx.utils.aoa_to_sheet(gainsParsed);
+            
+            xlsx.utils.book_append_sheet(wb, bit2sheet, 'cryptoSheet');
+            xlsx.utils.book_append_sheet(wb, mostActivesheet, 'mostActiveSheet');
+            xlsx.utils.book_append_sheet(wb, gainssheet, 'TopGainerSheet');
+            xlsx.set_fs(fs);
+            xlsx.writeFile(wb, 'output.xlsx');
+        }catch(error){
+            
+        }
+
         await browser.close();
-
 
     }catch(error){
         console.log(error)
